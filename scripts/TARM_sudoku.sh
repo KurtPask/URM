682e2e9b5b7abd770e5a06a599ae8a56690d68a3
KurtPask <kurtpasque@gmail.com>
2026-05-24 18:43:56 -0700

    update run script

--- scripts/TARM_sudoku.sh
+++ scripts/TARM_sudoku.sh
@@ -1,5 +1,18 @@
 optimizer_name="schedulefree"  #"schedulefree" or "adam_atan2"
-run_name="TARM-sudoku_v3_tropicalgemm_loop16_H1_L32_layers1_gradaccum6_hidden512_head8_skedfree"
+lc=3
+hc=1
+hs=1024
+nh=8
+loop=48
+nl=1
+ga=1
+bs=768 
+drop=0
+lr=1e-3
+pn=sudoku
+ver=v3
+halt=False
+run_name="TARM-${pn}_${ver}_loop${loop}_H${hc}_L${lc}_layers${nl}_gradaccum${ga}_hidden${hs}_head${nh}_batch${bs}_lr${lr}_${optimizer_name}"
 checkpoint_path="checkpoints/${run_name}" 
 mkdir -p $checkpoint_path
 
@@ -7,14 +20,16 @@ export MASTER_PORT=$((20000 + (${SLURM_JOB_ID:-$$} % 20000)))
 
 torchrun --nproc-per-node $SLURM_GPUS_ON_NODE --master_port=${MASTER_PORT} pretrain.py \
     data_path=$HOME/TropicalURM/URM/data/sudoku-extreme-1k-aug-1000 \
-    arch=tarm arch.loops=16 arch.H_cycles=1 arch.L_cycles=32 arch.hidden_size=512 arch.num_heads=8 arch.num_layers=1 arch.tropical_attention_version=v3 \
+    arch=tarm arch.loops=$loop arch.H_cycles=$hc arch.L_cycles=$lc arch.hidden_size=$hs arch.num_heads=$nh arch.num_layers=$nl arch.tropical_attention_version=$ver \
     epochs=50000 \
     eval_interval=2000 \
-    project_name=arcagi \
-    lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=100 grad_accum_steps=6 \
+    project_name=$pn \
+    lr=$lr puzzle_emb_lr=$lr weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=$bs grad_accum_steps=$ga \
     +run_name=$run_name \
     +checkpoint_path=$checkpoint_path \
     +ema=True \
+    +arch.disable_halting=$halt \
+    +arch.q_dropout=$drop +arch.k_dropout=$drop +arch.v_dropout=$drop \
     optimizer_name=$optimizer_name \
     # +load_checkpoint=$HOME/TropicalURM/URM/checkpoints/URM-sudoku/step_83325.pt \
     # +load_filter_mismatched_shapes=True \
